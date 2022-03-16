3a1afe2df8b4ff8a82382c63d8c6a211fb38f753
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-16 21:36:19 +0600

    components prop practice added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const names = [
+  { name: 'Dynamicly Added Name' },
+  { name: 'This is 2' }
+]
+
+const nameStyle = {
+  color: 'blue',
+  backgroundColor: 'white',
+  padding: '20px'
+}
+
 function App() {
+  const peoples = ['abul', 'babul', 'sabul', 'kabul']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {
+        peoples.map(people => <li>{people}</li>)
+      }
+      {
+        peoples.map(people => <Person name={people}></Person>)
+      } */}
+      {
+        names.map(name => <Person name={name.name}></Person>)
+      }
+
+      {/* <Person name={peoples[0]} age="23"></Person>
+      <Person name={peoples[1]} age="24"></Person>
+      <Person name={peoples[2]} age="25"></Person> */}
+      <h1>I can also write Between them</h1>
+      <Person2 books="Bela furabar age"></Person2>
+      <Person2 books="Ebar vinno kichu hok"></Person2>
     </div>
   );
+}
+function Person(props) {
+  return (
+    <div className='person'>
+      <h1>Name: {props.name}</h1>
+      <p>Age: {props.age}</p>
+    </div>
+  )
+}
+function Person2(props) {
+  return (
+    <div className='container'>
+      <h1>{props.books}</h1>
+      <p>Type: Books</p>
+    </div>
+  )
 }
 
 export default App;
